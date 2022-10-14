859316a3373e74f92a2767a89157eaf06db25dce
Jiyan Epözdemir <jepozdemir@gmail.com>
2022-10-14 15:19:25 +0300

    initial commit.

---
+++ ShellCommands/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ShellCommands
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnNewDoc = new System.Windows.Forms.Button();
+            this.btnMap = new System.Windows.Forms.Button();
+            this.btnExplorer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(25, 21);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Calculator";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnNewDoc
+            // 
+            this.btnNewDoc.Location = new System.Drawing.Point(106, 50);
+            this.btnNewDoc.Name = "btnNewDoc";
+            this.btnNewDoc.Size = new System.Drawing.Size(75, 23);
+            this.btnNewDoc.TabIndex = 1;
+            this.btnNewDoc.Text = "Mic.Word";
+            this.btnNewDoc.UseVisualStyleBackColor = true;
+            this.btnNewDoc.Click += new System.EventHandler(this.btnNewDoc_Click);
+            // 
+            // btnMap
+            // 
+            this.btnMap.Location = new System.Drawing.Point(106, 21);
+            this.btnMap.Name = "btnMap";
+            this.btnMap.Size = new System.Drawing.Size(75, 23);
+            this.btnMap.TabIndex = 2;
+            this.btnMap.Text = "Map Network Drive";
+            this.btnMap.UseVisualStyleBackColor = true;
+            this.btnMap.Click += new System.EventHandler(this.btnMap_Click);
+            // 
+            // btnExplorer
+            // 
+            this.btnExplorer.Location = new System.Drawing.Point(25, 50);
+            this.btnExplorer.Name = "btnExplorer";
+            this.btnExplorer.Size = new System.Drawing.Size(75, 23);
+            this.btnExplorer.TabIndex = 3;
+            this.btnExplorer.Text = "Microsoft.com";
+            this.btnExplorer.UseVisualStyleBackColor = true;
+            this.btnExplorer.Click += new System.EventHandler(this.btnExplorer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Controls.Add(this.btnExplorer);
+            this.Controls.Add(this.btnMap);
+            this.Controls.Add(this.btnNewDoc);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnNewDoc;
+        private System.Windows.Forms.Button btnMap;
+        private System.Windows.Forms.Button btnExplorer;
+    }
+}
+
---
+++ ShellCommands/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShellCommands
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.EnableRaisingEvents = false;
+            proc.StartInfo.FileName = "calc";
+            proc.Start();
+        }
+
+        private void btnMap_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.EnableRaisingEvents = false;
+            proc.StartInfo.FileName = "c:\\ShellTests\\netdrv.bat";
+            proc.Start();
+            MessageBox.Show("Map Drive Created");
+
+
+
+        }
+
+        private void btnExplorer_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+proc.EnableRaisingEvents=false;
+proc.StartInfo.FileName="iexplore";
+proc.StartInfo.Arguments="http://www.google.com/";
+proc.Start();
+proc.WaitForExit();
+MessageBox.Show("You have just visited www.microsoft.com");
+
+
+
+        }
+
+        private void btnNewDoc_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.EnableRaisingEvents = false;
+            proc.StartInfo.FileName = "winword";
+            proc.StartInfo.Arguments = "C:\\ShellTests\\TestDocument.doc";
+            proc.Start();
+
+
+
+        }
+    }
+}
